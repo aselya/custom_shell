b7365c009da5ef0c6bd14930199591903284b880
Aaron Selya <aarondavidselya@gmail.com>
2018-11-13 13:16:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 //
 //  main.cpp
-//  os_selya_project_shell
+//  
 //
-//  Created by Aaron David Selya on 11/15/17.
-//  Copyright © 2017 Aaron David Selya. All rights reserved.
-//
-//
+//  Created by Aaron David Selya on 11/13/18.
+//  Copyright © 2018 Aaron David Selya. All rights reserved.
 //
 //
-//Sources:
-//Pseudocode for a shell provided by Prof. Nourai in the assignmnet
+//
+//
+//Basic algorithim Pseudocode:
 //int main (int argc, char **argv) {
 //while (1) {
 //    printPrompt();
@@ -18,11 +17,6 @@
 //    executeCommand(cmd); /* command is either internal or external */
 //}}
 //
-//Several of the concepts utilized were detailed in the following tutorial provied by Prof Nourai in the assignment
-//https://brennan.io/2015/01/16/write-a-shell-in-c/
-//This program was created in C++ instead of C to demonstrate my understanding of the concepts detailed in the tutorial
-//to avoid any directly copied code from the tutorial.
-
 /////////////////////////////////////////////////////
 
 //libraries utilized
@@ -31,28 +25,23 @@
   struct Node {
     Node * next;
     std::string data;
-
   };
 
 //method that inserts the newest command at the end of the linked list
 //method returns the head node
 Node * Insert(Node * head, std::string data) {
-  printf("insert was called"); //string used in debugging
+  //printf("insert was called"); //string used in debugging
   Node * curr = new Node; // creates new node
   curr - > data = data; //puts the data into the string
   //curr->next = NULL; //sets the value of the next to null placing new node at end of the list
-
   if (head == NULL) { //if there is no head
     head = curr; //set head to new node
     printf("the head data is %s \n", head - > data.c_str());
-
   } else {
     curr - > next = head; //makes the next value connected to curr the current head
     head = curr; //sets curr to be the head
-
     printf("the head data is %s \n", head - > data.c_str());
   }
-
   return head;
 }
 
@@ -63,7 +52,6 @@ Node * printLinkedList(Node * head) {
   if (temp == NULL) { //catches no list exception
     printf("The history is empty\n");
   } else {
-
     static int count = 0; //sets count for printer output
     if (temp - > next == NULL) { //handeles one entry list exception
       printf("command 0 %s\n", head - > data.c_str());
@@ -71,15 +59,12 @@ Node * printLinkedList(Node * head) {
       while (temp - > next) { // while temp has next print it and the command number
         //printf("command %i : %s\n", count, temp->data.c_str());
         printf("command used: %s\n", temp - > data.c_str());
-
         count++; //increase count
         temp = temp - > next; //set next count variable
-
         if (temp - > next == NULL) { //checkts to see if this is the last node
           printf("command used: %s\n", temp - > data.c_str()); //if last, prints the data in node
         }
       }
-
     }
   }
   return head;
@@ -87,25 +72,21 @@ Node * printLinkedList(Node * head) {
 
 //this method prints the prompt shown for the shell;
 //if you want to change it just adjust the string
-
 void printThePrompt() {
   printf("AS>");
 }
 
 void read_command(char cmd[], char * par[]) {
-
   char line[1024]; //set variable for containing char array LINE since I don't care about memory it is larger than needed
   static int count = 0; //initializes count
   int i = 0; //initializes int
   char * array[100], * pch;
-
   for (;;) {
     int c = fgetc(stdin); //gets input from console
     line[count++] = (char) c; //puts each char from console into line array using for loop
     if (c == '\n') { // breaks at new line
       break;
     }
-
   }
   if (count == 1) {
     return;
@@ -116,12 +97,10 @@ void read_command(char cmd[], char * par[]) {
     array[i++] = strdup(pch);
     pch = strtok(NULL, " \n");
   }
-
   strcpy(cmd, array[0]);
 
   for (int j = 0; j < i; j++) {
     par[j] = array[j];
-
   }
   par[i] = NULL;
 
@@ -174,7 +153,6 @@ int main() {
       //following code will change value to appropriate call or it will remain as a error
       if (start == NULL) {
         printf("command not found");
-
       }
 
       int counter = 0; //counter starts at 0
@@ -188,11 +166,8 @@ int main() {
         }
         start = start - > next;
         counter++;
-
       }
-
       //if not found command is "error"
-
     }
 
     //built in commands are placed here
@@ -229,7 +204,6 @@ int main() {
         if (chdir(parameters[1]) != 0) {
           printf("error");
         }
-
       }
 
     } else if (strcmp(command, "") == 0) { //in the event there is an issue with the command initating a prompt
@@ -285,7 +259,5 @@ int main() {
 
     }
   }
-
   return status;
-
 }
